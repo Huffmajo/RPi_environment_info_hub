fcaa23646377bcf7eb91dbd021b8d4ca4292a156
Huffmajo <huffmajo@oregonstate.edu>
2019-09-16 02:52:12 +0000

    Added new function to display measured temperature and humidity from DHT11 sensor.

--- lcd.py
+++ lcd.py
@@ -4,10 +4,15 @@ from time import sleep, strftime
 from datetime import datetime
 import socket
 import RPi.GPIO as GPIO
+import Adafruit_DHT
 
 # initialize GPIO for buttons
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# initialize DHT11 sensor for local temperature and humidity
+temp_sensor = Adafruit_DHT.DHT11
+temp_gpio = 17
 
 # initialize LCD (must specify pinout and dimensions)
 lcd = Adafruit_CharLCD(rs=26, en=19, d4=13, d5=6, d6=5, d7=11, cols=16, lines=2)
@@ -24,6 +29,25 @@ def get_ip_address():
 def get_current_time():
 	return datetime.now().strftime('%b %d %H:%M:%S')
 
+# function to get humidity and temperature from DHT11
+def get_local_temp():
+	# let user know temps are being measured
+	lcd.message('Measuring local\ntemp/humidity...')
+
+	humidity, temp = Adafruit_DHT.read_retry(temp_sensor, temp_gpio)
+
+	# return error if DHT11 fails to detect humidity
+	if humidity is not None and temp is not None:
+		result = 'Temp: {0:0.1f}C\nHumidity: {1:0.1f}%'.format(temp, humidity)
+	else:
+		result = '***ERROR***\nNo temps read'
+
+	# clear screen before posting the data
+	lcd.clear()
+
+	# return tuple of weather data strings
+	return result
+
 # main control flow
 display = 0
 
@@ -33,20 +57,27 @@ try:
 		button_press = not GPIO.input(18)
 		if button_press:
 			display += 1
-			if display > 1:
+			if display > 2:
 				display = 0
 
 		lcd.clear()
+		# display IP info
 		if display == 0:
 			ip = get_ip_address()
 			lcd.message('IP ADDRESS\n')
 			lcd.message('{}'.format(ip))
 			sleep(1)
+		# display date and time
 		elif display == 1:
 			current_time = get_current_time()
 			lcd.message('TIME\n')
 			lcd.message(current_time)
 			sleep(1)
+		# display measured weather info
+		elif display == 2:
+			measured_temp = get_local_temp()
+			lcd.message(measured_temp)
+			sleep(3)
 
 except KeyboardInterrupt:
 	print('\nCTRL-C pressed. Program exiting...')
