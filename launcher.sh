9e6b52fc77a8bc24bac4e64e4151387ea52cdc86
Huffmajo <huffmajo@oregonstate.edu>
2019-12-08 21:17:27 -0800

    lcd.py now auto-runs on startup

---
+++ launcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# launcher.sh
+# cd to home directory, then to this directory, then run lcd.py, then back to home
+
+cd /
+cd home/pi/scripts/RPi_environment_info_hub
+sudo python lcd.py
+cd /
