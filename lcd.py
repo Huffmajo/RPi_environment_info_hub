bfc490511aaf3b40563d4da3691c38082808e08e
huffmajo <huffmajo@oregonstate.edu>
2020-04-05 17:35:28 -0700

    Removed unnecessary code. Ready to add valve control functionality

--- lcd.py
+++ lcd.py
@@ -107,18 +107,16 @@ def record_weather():
 
 	# get outside weather
 	weather_out = get_api_weather()
-	main = weather_out['main']
-	temp_out = (main['temp'] - 273) * 1.8 + 32
-	humid_out = main['humidity']
+	temp_out = (weather_out['main']['temp'] - 273) * 1.8 + 32
+	humid_out = ['main']['humidity']
 
 	# get weather description
-	weather_out =  weather_out['weather']
-	weather_desc = weather_out[0]['main']
+	weather_desc = weather_out['weather'][0]['description']
 
 	# get rainfall
-	if outside_weather['rain'] != null:
+	if "rain" in weather_out:
 		rain = outside_weather['rain']
-		rain_1h = rain['1h']
+		rain_1h = outside_weather['rain']['1h']
 	else:
 		rain_1h = 0
 
@@ -142,43 +140,19 @@ outside_humidity = 0
 rain_1h = 0
 description = 'None'
 
+# allow button presses to modify display
+left_button_press = not GPIO.input(left_button_gpio)
+right_button_press = not GPIO.input(right_button_gpio)
+power_button_press = not GPIO.input(power_button_gpio)
+
 try:
 	while True:
 		ms = int(round(time.time() * 1000))
 
-		# refresh weather information every 5 minutes
-		if ((ms - prev_temps > timer_temps_refresh) or prev_temps == 0):
-				# get inside temperature and humidity
-				inside_temp, inside_humidity = get_local_temp()
-
-				# get outside weather info
-				outside_weather = get_api_weather()
-				main = outside_weather['main']
-
-				# pull the weather data we need
-				outside_temp = main['temp']
-				outside_humidity = main['humidity']
-
-				# convert temp from kelvin to fahrenheit
-				outside_temp = (outside_temp - 273) * 1.8 + 32
-
-				# get weather description
-				weather_main = outside_weather['weather']
-				description = weather_main[0]['main']
-
-				# get recent rain levels
-				if outside_weather['rain'] != null:
-					rain = outside_weather['rain']
-					rain_1h = rain['1h']
-				else:
-					rain_1h = 0
-
-				prev_temps = ms
-
-		# allow button presses to modify display
-		left_button_press = not GPIO.input(left_button_gpio)
-		right_button_press = not GPIO.input(right_button_gpio)
-		power_button_press = not GPIO.input(power_button_gpio)
+		if (ms - prev_ms > timer_switch_screen):
+			lcd.clear();
+			lcd.message("***SCREEN ONE***\n")
+			prev_ms = ms
 
 		# test for solenoid triggering
 		if (right_button_press):
@@ -189,109 +163,6 @@ try:
 		# check for shutdown button press
 		if (power_button_press):
 			shutdown()
-
-		# button1 switches screens
-		if ((left_button_press) and (ms - prev_ms > timer_switch_screen)):
-			display += 1
-			if display > 2:
-				display = 0
-			prev_ms = int(round(time.time() * 1000))
-			print('Left_button_press_success. Display: {}'.format(display))
-
-		# welcome screen
-		if display == -1:
-			if (ms - prev_ms > timer_switch_screen):
-				lcd.clear()
-				lcd.message("***ENVIRO-HUB***\nPress any button")
-
-				# right button also exits welcome screen
-				if (right_button_press):
-					display += 1
-					print('Right_button_press_success. Display: {}'.format(display))
-				prev_ms = ms
-
-		# display datetime or IP
-		elif display == 0:
-			function = 1
-			if (ms - prev_ms > timer_IP_refresh):
-				# display date and time
-				if function == 1:
-					current_time = get_current_time()
-					lcd.clear()
-					lcd.message(current_time)
-				elif function == -1:
-					ip = get_ip_address()
-					lcd.clear()
-					lcd.message("IP address:\n{}".format(ip))
-				prev_ms = ms
-
-			# right button switches function
-			if ((right_button_press) and (ms - prev_ms > timer_switch_screen)):
-				if function == 1:
-					function = -1
-				else:
-					function = 1
-				prev_ms = int(round(time.time() * 1000))
-				print('Right_button_press_success. Display: {}'.format(display))
-				print('Function: {}'.format(function))
-
-		# display inside/outside temps and humidity
-		elif display == 1:
-			function = 1
-
-			if (ms - prev_ms > timer_IP_refresh):
-
-				# display temps
-				lcd.clear()
-				inside_temp_print = 'Inside: {0:0.1f}F'.format(inside_temp)
-				lcd.message(inside_temp_print)
-				lcd.message('\n')
-				outside_temp_print = 'Outside: {0:0.1f}F'.format(outside_temp)
-				lcd.message(outside_temp_print)
-
-				prev_ms = ms
-
-			# right button switches function
-			if ((Right_button_press) and (ms - prev_ms > timer_switch_screen)):
-				if function == 1:
-					function = -1
-				else:
-					function = 1
-				prev_ms = int(round(time.time() * 1000))
-				print('Right_button_press_success. Display: {}'.format(display))
-				print('Function: {}'.format(function))
-
-		# display outside weather info
-		elif display == 2:
-			function = 1
-
-			if (ms - prev_ms > timer_IP_refresh):
-
-				# display weather description and rainfall
-				if function == 1:
-					lcd.clear()
-					lcd.message(description)
-					lcd.message('\n')
-					rain_print = 'Rainfall: {0:0.1f}mm'.format(rain_1h)
-					lcd.message(rain_print)
-				# display humidity
-				elif function == -1:
-					lcd.clear()
-					inside_temp_print = 'Inside: {0:0.1f}%'.format(inside_humidity)
-					lcd.message(inside_temp_print)
-					lcd.message('\n')
-					outside_humidity_print = 'Outside: {0:0.1f}F'.format(outside_humidity)
-					lcd.message(outside_humidity_print)
-				prev_ms = ms
-
-			# right buttons switches function
-			if (right_button_press):
-				if function == 1:
-					function = -1
-				else:
-					function = 1
-				print('Right_button_press_success. Display: {}'.format(display))
-				print('Function: {}'.format(function))
 
 except KeyboardInterrupt:
 	print('\nCTRL-C pressed. Program exiting...')
